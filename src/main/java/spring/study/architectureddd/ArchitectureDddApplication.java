a7666965964f3f90724f06d54e7d4d26df46599b
홍준성 <powerstar13@kai-i.com>
2020-09-05 14:11:50 +0900

    git init > SpringBoot > Architecture ddd start

---
+++ src/main/java/spring/study/architectureddd/ArchitectureDddApplication.java
@@ -0,0 +1,13 @@
+package spring.study.architectureddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitectureDddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchitectureDddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/spring/study/architectureddd/ArchitectureDddApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.study.architectureddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitectureDddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
